a038279cb4d17160af28b3510a275680f8ca9f63
MihailPohodeev <kristinaschleikel@gmail.com>
2024-12-26 01:08:16 +0300

    chat fixes

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -19,33 +19,49 @@ class ChatChannel < ApplicationCable::Channel
     @@user_channels.delete(user_id)
   end
 
-  def receive(data)
-    contain = JSON.parse(data['message'])
-    chat = Chat.find_by(user_id: contain['myID'], advertisement_id: contain['advertisementID'])
-    if (chat.nil?)
-      chat = Chat.new(user_id: contain['myID'], advertisement_id: contain['advertisementID'])
-      chat.save
-    end
+  def send_message(data)
+    # contain = JSON.parse(data)
+    puts data['chat_id']
+    chat = Chat.find_by(id: data['chat_id'])
 
-    puts contain['myID']
-    puts contain['toID']
-    usr = User.find_by(id: contain['myID'])
-    msg = Message.new(content: contain['message'], chat: chat, user: usr, state: 'nonsend')
-    # puts msg.as_json()
+    usr = User.find_by(id: data['my_id'])
+    usr2 = Advertisement.find_by(id: data['adv_id']).user
+    msg = Message.new(content: data['content'], chat: chat, user: usr, state: 'nonsend')
+    puts data['content']
     msg.save
 
-    puts "user_#{contain['toID']}"
-    ActionCable.server.broadcast("user_#{contain['toID']}", { title: "message", body: usr.chats.as_json })
+    if (usr.id == usr2.id)
+      usr2 = chat.user
+    end
+    
+    ActionCable.server.broadcast("user_#{usr2.id}", { title: "message", body: msg.as_json })
 
     # Optionally, you can broadcast the message to all subscribers
     # ActionCable.server.broadcast("chat_channel", message: message)
   end
 
-  def get_my_chats(data)
+  def get_chat_id(data)
+    chat = Chat.find_by(user_id: data['my_id'], advertisement_id: data['adv_id'])
+    if chat.nil?
+      chat = Chat.new(user_id: data['my_id'], advertisement_id: data['adv_id'])
+      chat.save
+    end
+    ActionCable.server.broadcast("user_#{data['my_id']}", { title: "current_chat", body: chat.as_json })
+  end
+
+  def get_my_founds_chats(data)
     # puts data['userID']
     user_id = data['userID']
     chats = Chat.joins(:advertisement)
-            .where("chats.user_id = ? OR advertisements.user_id = ?", user_id, user_id)
+            .where("chats.user_id = ?", user_id)
+    ActionCable.server.broadcast("user_#{user_id}", { title: "chats", body: chats.as_json })
+  end
+
+  def get_my_advs_chats(data)
+    # puts data['userID']
+    user_id = data['userID']
+    chats = Chat.joins(:advertisement)
+            .where("advertisements.user_id = ?", user_id)
     ActionCable.server.broadcast("user_#{user_id}", { title: "chats", body: chats.as_json })
   end
 
@@ -54,12 +70,12 @@ class ChatChannel < ApplicationCable::Channel
     # puts user.as_json
     # adv = Advertisement.find_by(id: data['advertisementID'])
     # puts adv.as_json
-    chat = Chat.find_by(user_id: data['userID'], advertisement_id: data['advertisementID'])
+    chat = Chat.find_by(id: data['chat_id'])
     if chat.nil?
       puts 'BEDA'
     else
       messages = chat.messages
-      ActionCable.server.broadcast("user_#{data['userID']}", {title: "list_of_messages", body: messages.as_json})
+      ActionCable.server.broadcast("user_#{data['my_id']}", {title: "list_of_messages", body: messages.as_json})
     end
   end
 end
